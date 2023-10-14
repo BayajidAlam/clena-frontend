e8fefab7a3329b8277ea66f5d711a9e857ab3cb2
BayajidAlam <bayajidalam2001@gmail.com>
2023-10-15 02:28:51 +0600

    footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import ClientReview from "@/components/Home/ClientReview";
 import EventsByCategory from "@/components/Home/EventsByCategory";
+import Footer from "@/components/Home/Footer";
 import HomeHeader from "@/components/Home/HomeHeader";
 import HomeService from "@/components/Home/HomeService";
 import LatestNews from "@/components/Home/LatestNews";
 import OverView from "@/components/Home/OverView";
 import TopBanner from "@/components/Home/TopBanner";
 import UpcomingService from "@/components/Home/UpcomingService";
-import { Footer } from "antd/es/layout/layout";
-import { redirect } from "next/navigation";
-// return redirect("/super_admin/admin");
+
+
 const HomePage = () => {
   return (
     <div>
